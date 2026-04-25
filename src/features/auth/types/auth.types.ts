eb8ee2130e004b0d5ae781f7669984dda2134ebb
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-04-25 12:09:16 +0600

    auth signip shema

---
+++ src/features/auth/types/auth.types.ts
@@ -0,0 +1,14 @@
+export interface SignupInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: {
+    id: string;
+    email: string;
+    name: string;
+  };
+}
